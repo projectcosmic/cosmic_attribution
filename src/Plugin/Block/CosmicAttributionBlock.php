e38132ccfc0130cf76a7ade7fd85adcad9de103b
Justin Wong <justin@cosmic.org.uk>
2018-04-20 14:01:40 +0100

    Initial commit

---
+++ src/Plugin/Block/CosmicAttributionBlock.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\cosmic_attribution\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'CosmicAttributionBlock' block.
+ *
+ * @Block(
+ *  id = "cosmic_attribution_block",
+ *  admin_label = @Translation("Cosmic attribution block"),
+ * )
+ */
+class CosmicAttributionBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return [
+      '#theme' => 'cosmic_logo',
+      '#attached' => [
+        'library' => [
+          'cosmic_attribution/cosmic-logo',
+        ],
+      ],
+    ];
+  }
+
+}
